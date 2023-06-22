6359f5e2b03a76d9b8e5f2602b8919b6435b6ce0
vmrozhkovsky <128725885+vmrozhkovsky@users.noreply.github.com>
2023-06-22 17:29:43 +0300

    Добавлен код проекта PerfomanceTest

--- PerfomanceTest/Program.cs
+++ PerfomanceTest/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Diagnostics;
 
-Console.WriteLine("Hello, World!");
+string text = File.ReadAllText("text1.txt");
+List<char> lstChar = new List<char>();
+LinkedList<char> linkedListChar = new LinkedList<char>();
+
+TestListPertfomance();
+TestLinkedListPertfomance();
+
+void TestListPertfomance()
+{
+    Stopwatch stopWatch = Stopwatch.StartNew();
+    foreach (char ch in text)
+    {
+        lstChar.Add(ch);
+    }
+    Console.WriteLine(stopWatch.ElapsedMilliseconds); 
+}
+void TestLinkedListPertfomance()
+{
+    Stopwatch stopWatch = Stopwatch.StartNew();
+    foreach (char ch in text)
+    {
+        linkedListChar.AddLast(ch);
+    }
+    Console.WriteLine(stopWatch.ElapsedMilliseconds); 
+}
